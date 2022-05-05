fb9c8e322261c8e8a6ed00739683edeb9d2a230f
Eknathreddy09 <eknath.reddy09@gmail.com>
2022-05-05 19:12:31 +0530

    Update HelloControllerTest.java

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -26,6 +26,6 @@ class HelloControllerTest {
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Eknath"));
+            .andExpect(content().string("Greetings from Spring Boot + Tanzu!"));
     }
 }
