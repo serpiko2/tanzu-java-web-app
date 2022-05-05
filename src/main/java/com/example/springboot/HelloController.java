d6ef7724ef39bccdfdb8f2b6fc5d8126db050172
Eknathreddy09 <eknath.reddy09@gmail.com>
2022-05-05 19:26:29 +0530

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + EknathReddy";
+		return "Greetings from Spring Boot + Welcome to Partner demo";
 	}
 
 }
