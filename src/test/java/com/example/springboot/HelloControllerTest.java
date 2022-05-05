44f99c40056cbfe7c7efbbff667bdcff3d1fe8c0
Eknathreddy09 <eknath.reddy09@gmail.com>
2022-05-05 19:26:42 +0530

    Update HelloControllerTest.java

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + EknathReddy", controller.index());
+        assertEquals("Greetings from Spring Boot + Welcome to Partner demo", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + EknathReddy"));
+            .andExpect(content().string("Greetings from Spring Boot + Welcome to Partner demo"));
     }
 }
