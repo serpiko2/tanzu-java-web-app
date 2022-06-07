3822d98e67a0c5b225d6610e7e1e30ae4de9d49a
serpiko2 <79837722+serpiko2@users.noreply.github.com>
2022-06-08 00:35:31 +0200

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Welcome to Partner demo";
+		return "Greetings from Spring Boot + Welcome to demo";
 	}
 
 }
